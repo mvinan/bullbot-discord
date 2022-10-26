a04102d908f53b5c7cb4c455a3c9fe678b47e779
Miguel Vinan <a@miguelvinan.com>
2022-10-26 07:19:42 -0500

    feature: add nodemon + yarn to project

--- index.js
+++ index.js
@@ -2,36 +2,47 @@ const createCommands = require("./services/create-commands.js");
 const { Events } = require("discord.js");
 const client = require("./client.js");
 const { token } = require("./config.js");
+const express = require("express");
+const path = require("path");
+const app = express();
 
-(function () {
-  createCommands();
+createCommands();
 
-  client.on(Events.InteractionCreate, async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
+const port = process.env.PORT || 8080;
 
-    const command = interaction.client.commands.get(interaction.commandName);
+const health = (req, res) => {
+  res.status(200).json({ health: "OK" });
+};
 
-    if (!command) {
-      console.error(
-        `No command matching ${interaction.commandName} was found.`
-      );
-      return;
-    }
+app.use("/healthz", health);
 
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    }
-  });
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
 
-  client.once(Events.ClientReady, () => {
-    console.log("Ready!");
-  });
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
-  client.login(token);
-})();
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+
+client.once(Events.ClientReady, () => {
+  console.log("Ready!");
+});
+
+client.login(token);
